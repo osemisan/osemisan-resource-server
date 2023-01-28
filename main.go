293d73ffc66ca4333651353307d6ce0c3eda864e
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-01-28 13:57:16 +0900

    Read scope from context

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -18,12 +19,12 @@ type semiResource struct {
 	length string
 }
 
-var semiResources = []semiResource{
-	{"アブラゼミ", "5cm"},
-	{"ミンミンゼミ", "3.5cm"},
-	{"クマゼミ", "7cm"},
-	{"ニイニイゼミ", "2cm"},
-	{"ツクツクボウシ", "4.5cm"},
+var semiResources = map[string]semiResource{
+	"abura": {"アブラゼミ", "5cm"},
+	"minmin": {"ミンミンゼミ", "3.5cm"},
+	"kuma": {"クマゼミ", "7cm"},
+	"niinii": {"ニイニイゼミ", "2cm"},
+	"tsukutsuku": {"ツクツクボウシ", "4.5cm"},
 }
 
 type semiScopes struct {
@@ -85,7 +86,32 @@ func main() {
 	})
 
 	r.Get("/resources", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("osemisan"))
+		resources := make([]semiResource, 0, 5)
+
+		if r.Context().Value("abura").(bool) {
+			resources = append(resources, semiResources["abura"])
+		}
+		if r.Context().Value("minmin").(bool) {
+			resources = append(resources, semiResources["minmin"])
+		}
+		if r.Context().Value("kuma").(bool) {
+			resources = append(resources, semiResources["kuma"])
+		}
+		if r.Context().Value("niinii").(bool) {
+			resources = append(resources, semiResources["niinii"])
+		}
+		if r.Context().Value("tsukutsuku").(bool) {
+			resources = append(resources, semiResources["tsukutsuku"])
+		}
+
+		jsonRes, err := json.Marshal(resources)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonRes)
 	})
 
 	http.ListenAndServe(":9002", r)
